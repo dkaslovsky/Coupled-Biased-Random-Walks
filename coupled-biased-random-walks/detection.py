e733fc06a9d641b55af3f723bd2836de406906a5
dkaslovsky <dkaslovsky@gmail.com>
2018-07-09 23:22:40 -0600

    random walk

--- coupled-biased-random-walks/detection.py
+++ coupled-biased-random-walks/detection.py
@@ -1,18 +1,27 @@
 from __future__ import division
 
+import numpy as np
 from scipy.sparse import csr_matrix
 from six import iteritems
-from six.moves import zip
+from six.moves import range, zip
 
 from count import ObservationCounter
 
 
 class CBRW(object):
 
-    def __init__(self):
+    preset_rw_params = {
+        'alpha':    0.95,
+        'err_tol':  1e-3,
+        'max_iter': 100
+    }
+
+    def __init__(self, rw_params=None):
+        self.rw_params = rw_params if rw_params else self.preset_rw_params
         self._counter = ObservationCounter()
         self._trans_matrix = None
         self._bias_dict = None
+        self._trans_prob = None
 
     def add_observations(self, observation_iterable):
         self._counter.update(observation_iterable)
@@ -20,8 +29,9 @@ class CBRW(object):
     def fit(self):
         if self._counter.n_obs == 0:
             raise ValueError('no observations provided')
-        self._compute_biases()
-        self._compute_trans_matrix()
+        self._bias_dict = self._compute_biases()
+        self._trans_matrix = self._compute_trans_matrix()
+        self._trans_prob = self._random_walk()
         return self
 
     def _compute_trans_matrix(self):
@@ -46,7 +56,7 @@ class CBRW(object):
 
         n_symb = len(self._counter.index)
         trans_matrix = csr_matrix((prob, zip(*idx)), shape=(n_symb, n_symb))
-        self._trans_matrix = self._row_normalize_csr_matrix(trans_matrix)
+        return self._row_normalize_csr_matrix(trans_matrix)
 
     def _compute_biases(self):
         bias_dict = {}
@@ -55,7 +65,28 @@ class CBRW(object):
             base = 1 - mode / self._counter.n_obs
             bias_dict.update({feature_val: self._compute_bias(count, mode, base)
                               for feature_val, count in iteritems(value_counts)})
-        self._bias_dict = bias_dict
+        return bias_dict
+
+    def _random_walk(self):
+        # get random walk parameters
+        alpha = self.rw_params['alpha']
+        err_tol = self.rw_params['err_tol']
+        max_iter = self.rw_params['max_iter']
+
+        # shape of transition matrix will be length of vectors
+        n = self._trans_matrix.shape[0]
+        # damping vector
+        damping_vec = ((1 - alpha) / n) * np.ones((n, 1))
+        # stationary vector initialization
+        pi = (1 / n) * np.ones((n, 1))
+
+        for _ in range(max_iter):
+            pi_next = damping_vec + alpha * self._trans_matrix.T.dot(pi)
+            err = np.linalg.norm(pi - pi_next, ord=np.inf)
+            if err <= err_tol:
+                return pi_next
+            pi = pi_next
+        return pi
 
     @staticmethod
     def _compute_bias(count, mode, base):
