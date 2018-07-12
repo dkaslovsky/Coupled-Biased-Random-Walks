743e7e353d0a43fc4615fa4985ba72787fa29157
dkaslovsky <dkaslovsky@gmail.com>
2018-07-11 21:47:30 -0600

    extend Mapping

--- coupled-biased-random-walks/count.py
+++ coupled-biased-random-walks/count.py
@@ -3,31 +3,36 @@ from itertools import combinations, tee
 
 from six import iteritems
 
+try:
+    # python 2
+    from collections import Mapping
+except ImportError:
+    # python 3
+    from collections.abc import Mapping
 
-class FixedValueDict(dict):
-    def __setitem__(self, key, value):
-        if dict.has_key(self, key):
-            raise KeyError('cannot overwrite key {}'.format(key))
-        dict.__setitem__(self, key, value)
 
+class IncrementingDict(Mapping):
 
-class IncrementingDict(object):
+    """
+    Inherit abstract base class Mapping instead of dict; note that we
+    intentionally do not define a __setitem__ method
+    """
+
     def __init__(self):
+        self._d = {}
         self._next_val = 0
-        self._d = FixedValueDict()
 
     def insert(self, key):
-        try:
-            self._d[key] = self._next_val
-        except KeyError:
+        if self._d.has_key(key):
             return
+        self._d[key] = self._next_val
         self._next_val += 1
 
-    def get(self, key, default=None):
-        return self._d.get(key, default)
-
     def __getitem__(self, key):
-        return self._d.__getitem__(key)
+        return self._d[key]
+
+    def __iter__(self):
+        return self._d.__iter__()
 
     def __len__(self):
         return self._d.__len__()
