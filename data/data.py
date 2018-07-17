a715e8b862959d6a7d933ab3dbb8cdca38d13ab3
dkaslovsky <dkaslovsky@gmail.com>
2018-07-16 22:11:19 -0600

    example csv

---
+++ data/data.py
@@ -0,0 +1,18 @@
+from csv import DictReader
+from six import iteritems
+from functools import partial
+
+
+def load_from_csv(path_to_csv, exclude_cols=None):
+    with open(path_to_csv, 'r') as csvfile:
+        data = list(DictReader(csvfile))
+    if exclude_cols is not None:
+        if isinstance(exclude_cols, str):
+            exclude_cols = {exclude_cols}
+        filt = partial(filter_fields, set(exclude_cols))
+        return [filt(rec) for rec in data]
+    return data
+
+
+def filter_fields(record, fields):
+    return {k: v for k, v in iteritems(record) if k not in fields}
