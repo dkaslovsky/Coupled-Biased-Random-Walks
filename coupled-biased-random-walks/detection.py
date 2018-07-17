9cd98117178981fe194c06fd34eea8c609f35b7e
dkaslovsky <dkaslovsky@gmail.com>
2018-07-16 21:25:19 -0600

    feature relevance weights

--- coupled-biased-random-walks/detection.py
+++ coupled-biased-random-walks/detection.py
@@ -7,7 +7,7 @@ from scipy.sparse import csr_matrix
 from six import iteritems
 from six.moves import range, zip
 
-from count import ObservationCounter
+from count import ObservationCounter, get_feature_name, get_mode
 
 
 class CBRW(object):
@@ -21,10 +21,8 @@ class CBRW(object):
     def __init__(self, rw_params=None):
         self.rw_params = rw_params if rw_params else self.PRESET_RW_PARAMS
         self._counter = ObservationCounter()
-        self._transition_matrix = None
-        self._bias_dict = None
         self._stationary_prob = None
-        self._feature_weights = None
+        self._feature_relevance = None
 
     def add_observations(self, observation_iterable):
         self._counter.update(observation_iterable)
@@ -32,9 +30,9 @@ class CBRW(object):
     def fit(self):
         if self._counter.n_obs == 0:
             raise ValueError('no observations provided')
-        self._bias_dict = self._compute_biases()
-        self._transition_matrix = self._compute_transition_matrix()
-        self._stationary_prob, self._feature_weights = self._compute_stationary_prob()
+        bias_dict = self._compute_biases()
+        transition_matrix = self._compute_biased_transition_matrix(bias_dict)
+        self._fit(transition_matrix)
         return self
 
     def score(self, observation_iterable):
@@ -42,8 +40,24 @@ class CBRW(object):
             observation_iterable = [observation_iterable]
         return np.array([self._score(obs) for obs in observation_iterable])
 
+    def _fit(self, transition_matrix):
+        pi = self._random_walk(transition_matrix, self.rw_params)
+        pi = pi.ravel()
+        stationary_prob = {}
+        feature_relevance = defaultdict(int)
+        for feature_val, idx in iteritems(self._counter.index):
+            prob = pi[idx]
+            stationary_prob[feature_val] = prob
+            feature_relevance[get_feature_name(feature_val)] += prob
+        self._stationary_prob = stationary_prob
+        # feature relevance scores are to be used as weights; accordingly the paper
+        # normalizes them to sum to 1, however this sum normalization should not be
+        # necessary since su(pi) = 1 by definition
+        self._feature_relevance = dict(feature_relevance)
+
     def _score(self, observation):
-        return sum(self._get_node_score(item) for item in iteritems(observation))
+        return sum(self._get_feature_relevance(item) * self._get_node_score(item)
+                   for item in iteritems(observation))
 
     def _get_node_score(self, node_name):
         try:
@@ -51,7 +65,11 @@ class CBRW(object):
         except KeyError:
             raise ValueError('unknown feature value: {}'.format(node_name))
 
-    def _compute_transition_matrix(self):
+    def _get_feature_relevance(self, feature_tuple):
+        feature_name = get_feature_name(feature_tuple)
+        return self._feature_relevance.get(feature_name, 0)
+
+    def _compute_biased_transition_matrix(self, bias_dict):
         idx = []
         prob = []
         for (symbol1, symbol2), joint_count in iteritems(self._counter.joint_counts):
@@ -66,10 +84,10 @@ class CBRW(object):
 
             # p(symb1 | symb2)
             idx.append((symb1_idx, symb2_idx))
-            prob.append(self._bias_dict[symbol2] * joint_count / symb2_count)
+            prob.append(bias_dict[symbol2] * joint_count / symb2_count)
             # p(symb2 | symb1)
             idx.append((symb2_idx, symb1_idx))
-            prob.append(self._bias_dict[symbol1] * joint_count / symb1_count)
+            prob.append(bias_dict[symbol1] * joint_count / symb1_count)
 
         n_symb = len(self._counter.index)
         trans_matrix = csr_matrix((prob, zip(*idx)), shape=(n_symb, n_symb))
@@ -78,38 +96,32 @@ class CBRW(object):
     def _compute_biases(self):
         bias_dict = {}
         for feature_name, value_counts in iteritems(self._counter.counts):
-            mode = self._get_mode(value_counts)
+            mode = get_mode(value_counts)
             base = 1 - mode / self._counter.n_obs
             bias_dict.update({feature_val: self._compute_bias(count, mode, base)
                               for feature_val, count in iteritems(value_counts)})
         return bias_dict
 
-    # TODO: rename, normalize, use self._counter.counts to get nested structure?
-    def _compute_stationary_prob(self):
-        pi = self._random_walk().ravel()
-        stationary_prob = {}
-        feature_weights = defaultdict(int)
-        for feature_val, idx in iteritems(self._counter.index):
-            prob = pi[idx]
-            stationary_prob[feature_val] = prob
-            feature_weights[self._counter._get_feature_name(feature_val)] += prob
-        return stationary_prob, dict(feature_weights)
-
-    def _random_walk(self):
-        # get random walk parameters
-        alpha = self.rw_params['alpha']
-        err_tol = self.rw_params['err_tol']
-        max_iter = self.rw_params['max_iter']
+    @staticmethod
+    def _random_walk(transition_matrix, params):
+        try:
+            # get random walk parameters
+            alpha = params['alpha']
+            err_tol = params['err_tol']
+            max_iter = params['max_iter']
+        except KeyError:
+            raise ValueError('one or more of alpha, err_tol, and max_iter '
+                             'missing from ranom walk parameter dict')
 
         # shape of transition matrix will be length of vectors
-        n = self._transition_matrix.shape[0]
+        n = transition_matrix.shape[0]
         # damping vector
         damping_vec = ((1 - alpha) / n) * np.ones((n, 1))
         # stationary vector initialization
         pi = (1 / n) * np.ones((n, 1))
 
         for _ in range(max_iter):
-            pi_next = damping_vec + alpha * self._transition_matrix.T.dot(pi)
+            pi_next = damping_vec + alpha * transition_matrix.T.dot(pi)
             err = np.linalg.norm(pi - pi_next, ord=np.inf)
             if err <= err_tol:
                 return pi_next
@@ -122,14 +134,10 @@ class CBRW(object):
         return 0.5 * (dev + base)
 
     @staticmethod
-    def _get_mode(counter_obj):
-        return counter_obj.most_common(1)[0][1]
-
-    @staticmethod
     def _row_normalize_csr_matrix(matrix):
         """
         Row normalize a csr matrix without mutating the input
-        :param matrix:
+        :param matrix: scipy.sparse.csr_matrix instance
         """
         # get row index for every nonzero element in matrix
         row_idx, col_idx = matrix.nonzero()
